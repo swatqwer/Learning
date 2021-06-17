f193c6402417ac7eb977fff2190f0f3732dc008f
Anatoliy <swatqwer@gmail.com>
2021-06-17 14:57:44 +0700

    Домашнее задание к 1 уроку.

---
+++ Learing-1/Learing-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            String name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Привет, {name}! \nСегодня {DateTime.Now}");
+            Console.ReadKey();
+        }
+    }
+}
